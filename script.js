9d93e4efd32ed2876658f7b465f72e903724c192
Grant Ombongi <grantthesoftwareengineer@gmail.com>
2026-01-15 11:46:59 +0300

    Added about section and navbar transluscent

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+//Scroll dynamism feature
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    const windowHeight = window.innerHeight;
+    const revealPoint = 120;
+
+    reveals.forEach((el) => {
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - revealPoint) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
