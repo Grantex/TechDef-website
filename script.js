492fbb655cded93c3c7c7d44b6addf92ccdd6aef
Grant Ombongi <grantthesoftwareengineer@gmail.com>
2026-01-15 12:02:53 +0300

    services section

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ navLinks.forEach(link => {
         navMenu.classList.remove("active");
     });
 });
-
+document.addEventListener("click", (e) => {
+    if (
+        !navMenu.contains(e.target) &&
+        !menuToggle.contains(e.target)
+    ) {
+        navMenu.classList.remove("active");
+    }
+});
 
 //Scroll dynamism feature
 
